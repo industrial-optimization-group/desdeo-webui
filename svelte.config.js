ab1cda4dc27d2c6174a014edebe69766b624232b
rmojala <109177275+rmojala@users.noreply.github.com>
2023-05-15 13:09:48 +0300

    Add prettier-plugin-jsdoc

--- svelte.config.js
+++ svelte.config.js
@@ -2,7 +2,7 @@
 import adapter from "@sveltejs/adapter-static";
 import { vitePreprocess } from "@sveltejs/kit/vite";
 
-/** @type {import('@sveltejs/kit').Config} */
+/** @type {import("@sveltejs/kit").Config} */
 const config = {
   // See https://kit.svelte.dev/docs/integrations#preprocessors
   preprocess: vitePreprocess(),
