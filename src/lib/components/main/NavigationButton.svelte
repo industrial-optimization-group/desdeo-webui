6443499507629d552f0d86fc8177964d14b99996
rmojala <109177275+rmojala@users.noreply.github.com>
2023-05-17 20:41:06 +0300

    Add basic navigation

---
+++ src/lib/components/main/NavigationButton.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+  export let href: string;
+  export let image: string;
+  export let text: string[];
+</script>
+
+<a {href}>
+  <div id="container">
+    <img src={image} alt="" />
+    <div id="text">
+      {#each text as row}
+        <span>{row}</span>
+      {/each}
+    </div>
+  </div>
+</a>
+
+<style>
+  #container,
+  #text {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  #container {
+    gap: 0.5em;
+  }
+
+  a {
+    color: #f0efef;
+    text-decoration-line: none;
+  }
+</style>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,45 @@
+<script>
+  import NavigationBar from "$lib/components/main/NavigationBar.svelte";
+</script>
+
+<div id="layout">
+  <nav>
+    <NavigationBar />
+  </nav>
+  <header>Top bar</header>
+  <main>
+    <slot />
+  </main>
+</div>
+
+<style>
+  :global(body) {
+    margin: 0;
+    background-color: #f0efef;
+  }
+
+  #layout {
+    display: grid;
+    grid-template-areas:
+      "a b"
+      "a c";
+    grid-template-columns: auto 1fr;
+    grid-template-rows: auto 1fr;
+
+    height: 100vh;
+  }
+
+  nav {
+    grid-area: a;
+  }
+
+  header {
+    grid-area: b;
+  }
+
+  main {
+    grid-area: c;
+
+    padding: 10px;
+  }
+</style>
