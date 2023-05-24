f4a5ce4ae18d7d1d2526c6bc4c58740c72b657ec
edciriac <65717975+edciriac@users.noreply.github.com>
2023-05-24 17:01:13 +0300

    Use data and name values dynamically

--- src/lib/components/visual/ParallelAxis.svelte
+++ src/lib/components/visual/ParallelAxis.svelte
@@ -3,34 +3,59 @@
   import { onMount } from "svelte";
 
   export let title: string;
-  export let data: (string | number)[][];
+  // export let data: (string | number)[][];
+  export let names: string[];
+  export let values: number[][];
+  //   export let dimensions: ({
+  //     dim: number;
+  //     name: string;
+  //     type?: undefined;
+  //     data?: undefined;
+  // } | {
+  //     dim: number;
+  //     name: string;
+  //     type: string;
+  //     data: string[];
+  // })[]
 
   onMount(() => {
     const chart = echarts.init(
       document.getElementById("chart") as HTMLCanvasElement
     );
+    let valueSeries: {
+      type: string;
+      data: number[][];
+    }[] = [];
+
+    console.log(values);
+    values.forEach((valueSet) => {
+      console.log(valueSet);
+      let valueObj = {
+        type: "parallel",
+        data: [valueSet],
+      };
+      valueSeries.push(valueObj);
+    });
+
+    let nameAxis = [];
+    for (let i = 0; i < names.length; i++) {
+      const name = names[i];
+      let nameObj = {
+        dim: i,
+        name: name,
+      };
+      nameAxis.push(nameObj);
+    }
+
+    console.log("valueseries:");
+    console.log(valueSeries);
+
     let option: echarts.EChartsOption = {
       title: {
         text: title,
       },
-      parallelAxis: [
-        { dim: 0, name: "Price" },
-        { dim: 1, name: "Net Weight" },
-        { dim: 2, name: "Amount" },
-        {
-          dim: 3,
-          name: "Score",
-          type: "category",
-          data: ["Excellent", "Good", "OK", "Bad"],
-        },
-      ],
-      series: {
-        type: "parallel",
-        lineStyle: {
-          width: 4,
-        },
-        data: data,
-      },
+      parallelAxis: nameAxis,
+      series: valueSeries,
     };
 
     chart.setOption(option);
--- src/routes/eCharts/+page.svelte
+++ src/routes/eCharts/+page.svelte
@@ -1,15 +1,35 @@
 <script lang="ts">
   import ParallelAxis from "$lib/components/visual/ParallelAxis.svelte";
 
-  let data = [
-    [12.99, 100, 82, "Good"],
-    [9.99, 80, 77, "OK"],
-    [20, 120, 60, "Excellent"],
-    [22, 125, 20, "Excellent"],
+  // let data = [
+  //   [12.99, 100, 82, "Good"],
+  //   [9.99, 80, 77, "OK"],
+  //   [20, 120, 60, "Excellent"],
+  //   [22, 125, 20, "Excellent"],
+  // ];
+
+  let names = ["Social", "Economic", "Environmental"];
+  let values = [
+    [-1.4563, -0.89456, -2.6478],
+    [-1.1653, -1.02342, -2.234],
+    [-1.003, -0.73453, -2.0785],
+    [-0.003, -0.453, -2.015],
   ];
+
+  // let dimensions = [
+  //   { dim: 0, name: "grrr" },
+  //   { dim: 1, name: "Net Weight" },
+  //   { dim: 2, name: "Amount" },
+  //   {
+  //     dim: 3,
+  //     name: "Score",
+  //     type: "category",
+  //     data: ["Excellent", "Good", "OK", "Bad"],
+  //   },
+  // ];
   let title = "test Title";
 </script>
 
 <div>
-  <ParallelAxis {title} {data} />
+  <ParallelAxis {title} {names} {values} />
 </div>
