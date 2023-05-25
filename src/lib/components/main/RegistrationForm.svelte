d0350436a570f1e5ce3c3a690079db43e9e9ac98
rmojala <109177275+rmojala@users.noreply.github.com>
2023-05-25 17:45:45 +0300

    Add registration form

---
+++ src/lib/components/main/RegistrationForm.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  export let username = "";
+  export let password = "";
+
+  let retyped = "";
+</script>
+
+<div class="flex flex-col items-center gap-8">
+  <div class="mb-6 text-2xl">
+    Register a <span class="underline">Free</span> Account
+  </div>
+  <form class="flex w-72 flex-col gap-4">
+    <input type="text" bind:value={username} placeholder="Username" />
+    <input type="password" bind:value={password} placeholder="Password" />
+    <input type="password" bind:value={retyped} placeholder="Retype password" />
+    <button class="btn variant-filled-primary mt-2 uppercase">Register</button>
+  </form>
+  <div class="flex flex-col items-center">
+    <span>Already have an account? </span>
+    <span
+      ><a href="login" class="anchor">Log in</a> or
+      <a href="/TODO" class="anchor">explore DESDEO as a guest</a></span
+    >
+  </div>
+</div>
---
+++ src/routes/(login)/+layout.svelte
@@ -0,0 +1,43 @@
+<script>
+  import { setContext } from "svelte";
+  import { writable } from "svelte/store";
+
+  // The "login" context is used to share the current values
+  // of the username and password inputs between the login form
+  // and the registration form. The point is to preserve
+  // these values if the user switches from one form to the other.
+  setContext("login", {
+    username: writable(""),
+    password: writable(""),
+  });
+
+  import logo from "$lib/assets/logo.png";
+</script>
+
+<div class="grid h-screen grid-cols-[1fr_auto]">
+  <div class="m-8 max-w-xl">
+    <div class="flex items-center gap-2">
+      <img src={logo} alt="" />
+      <span class="text-xl font-bold">DESDEO</span>
+    </div>
+    <div class="flex h-full items-center">
+      <div>
+        <h1 class="mb-12 text-5xl font-bold leading-snug">
+          Enabling Better<br />Decision Making
+        </h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur.
+        </p>
+      </div>
+    </div>
+  </div>
+
+  <div class="flex flex-col justify-center bg-white px-20 py-8">
+    <slot />
+  </div>
+</div>
