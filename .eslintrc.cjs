76ce09a55d7633dd4befb0c2ace205e185bb49ff
rmojala <109177275+rmojala@users.noreply.github.com>
2023-05-25 04:00:34 +0300

    Add eslint-plugin-tailwindcss

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,6 +4,7 @@ module.exports = {
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:svelte/recommended",
+    "plugin:tailwindcss/recommended",
     "prettier",
   ],
   parser: "@typescript-eslint/parser",
