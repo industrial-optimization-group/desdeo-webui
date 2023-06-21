6fd1ae0e77c52bf4df0e5b748c32386499c27ce9
edciriac <65717975+edciriac@users.noreply.github.com>
2023-06-21 22:22:36 +0300

    Add first version of highlighting on bar chart

--- src/lib/components/visual/barChart.svelte
+++ src/lib/components/visual/barChart.svelte
@@ -17,12 +17,15 @@
   const names: string[] = data.names;
   const values: number[][] = data.values;
 
+  let series = [];
+
   //
   let dataSet = [["Solution", ...names]];
   for (let i = 0; i < values.length; i++) {
     let newArr = ["Solution " + (i + 1)];
     newArr.push(...values[i]);
     dataSet.push(newArr);
+    series.push({ type: "bar" });
   }
   console.log(dataSet);
   onMount(() => {
@@ -46,6 +49,7 @@
       yAxis: {
         type: "value",
       },
+
       // series:
       // [
       // {
@@ -80,10 +84,45 @@
         source: dataSet,
         // sourceHeader: "auto",
       },
-      series: [{ type: "bar" }, { type: "bar" }, { type: "bar" }],
+      series: series,
     };
     // let chart: echarts.EChartsType = createChart(id, option);
-    createChart(id, option);
+    let chart = createChart(id, option);
+
+    chart.on("mouseover", (params) => {
+      console.log(params);
+      console.log(params.seriesIndex);
+      console.log(params.dataIndex);
+      chart.dispatchAction({
+        type: "downplay",
+        seriesIndex: [0, 1, 2],
+        dataIndex: [0, 1, 2],
+      });
+      chart.dispatchAction({
+        type: "highlight",
+        seriesIndex: params.seriesIndex,
+        dataIndex: [0, 1, 2],
+      });
+      chart.setOption({
+        emphasis: {
+          itemStyle: {
+            color: "red",
+          },
+        },
+      });
+    });
+    chart.on("mouseout", () => {
+      chart.dispatchAction({
+        type: "downplay",
+        seriesIndex: [0, 1, 2],
+        dataIndex: [0, 1, 2],
+      });
+      // chart.dispatchAction({
+      //   type: "highlight",
+      //   seriesIndex: 0,
+      //   dataIndex: params.dataIndex,
+      // });
+    });
   });
 </script>
 
