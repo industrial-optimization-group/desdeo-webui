846e36b756a047f2370343a18e2fcb1d87991775
Runo-Mikael Ojala <109177275+rmojala@users.noreply.github.com>
2023-06-21 11:25:58 +0300

    Improve login page visuals

--- src/routes/(login)/+layout.svelte
+++ src/routes/(login)/+layout.svelte
@@ -18,13 +18,13 @@
 </script>
 
 <div class="grid h-screen grid-cols-[1fr_auto]">
-  <div class="m-8 max-w-xl">
+  <div class="m-8">
     <div class="flex items-center gap-2">
       <img src={logo} alt="" />
       <span class="text-xl font-bold">DESDEO</span>
     </div>
-    <div class="flex h-full items-center">
-      <div>
+    <div class="mx-28 flex h-full items-center">
+      <div class="max-w-2xl">
         <h1 class="mb-12 text-5xl font-bold leading-snug">
           Enabling Better<br />Decision Making
         </h1>
