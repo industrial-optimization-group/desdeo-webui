6b7dcd9ebdc2d4928ab3759141c7d81cf13ac538
edciriac <edciriac@jyu.fi>
2023-06-29 15:44:37 +0300

    Modify horizontal bar and add test page for developing it

--- src/lib/components/visual/HorizontalAxisPlot.svelte
+++ src/lib/components/visual/HorizontalAxisPlot.svelte
@@ -5,11 +5,10 @@
 <script lang="ts">
   import * as echarts from "echarts";
   import { onMount } from "svelte";
-
+  export let id: string;
+  let chart;
   onMount(() => {
-    const chart = echarts.init(
-      document.getElementById("HorizontalAxisPlot") as HTMLCanvasElement
-    );
+    chart = echarts.init(document.getElementById(id) as HTMLCanvasElement);
     let option: echarts.EChartOption = {
       xAxis: {
         id: "xAxis",
@@ -49,30 +48,55 @@
         },
       ],
     };
-    chart.setOption(option);
 
+    addHoriBar(id, option);
+  });
+  $: value = 0;
+  function addHoriBar(id: string, option: echarts.EChartOption) {
+    chart.setOption(option);
     // Add event listener which adds and updates the line on the graph.
     chart.getZr().on("click", function (params) {
       // var pointInPixel = [params.offsetX, params.offsetY];
       // var pointInGrid = chart.convertFromPixel("grid", pointInPixel);
-      chart.setOption({
-        graphic: {
-          id: "rec",
-          x: params.offsetX,
-          shape: {
-            height: chart.getHeight(),
-          },
-        },
-      });
+      updateLine(params);
     });
-  });
+  }
+  function updateLine(params?) {
+    if (params.type === "click") {
+      value = chart.convertFromPixel({ seriesIndex: 0 }, [
+        params.offsetX,
+        params.offsetY,
+      ])[0];
+    }
+    const gridModel = chart.getModel().getComponent("grid");
+    const gridView = chart.getViewOfComponentModel(gridModel);
+    const gridRect = gridView.group.getBoundingRect();
+    // const xAxisModel = chart.getModel().getComponent("axisPointer");
+    // const xAxisView = chart.getViewOfComponentModel(xAxisModel);
+    // const xAxisRect = xAxisView.group.getBoundingRect();
+    // xAxisRect.height = chart.getHeight();
+    chart.setOption({
+      grid: {
+        show: true,
+        // height: chart.getHeight(),
+        // width: chart.getWidth(),
+        // position: [0,0]
+      },
+      graphic: {
+        id: "rec",
+        x: chart.convertToPixel({ seriesIndex: 0 }, [value, 0])[0],
+        // top: "center",
+        y: gridRect.y,
+        shape: {
+          height: gridRect.height,
+        },
+        transition: "all",
+      },
+    });
+  }
 </script>
 
-<div id="HorizontalAxisPlot" />
-
-<style>
-  #HorizontalAxisPlot {
-    width: 70vw;
-    height: 30vh;
-  }
-</style>
+<div>
+  <input type="text" bind:value on:change={updateLine} />
+  <div {id} style="width: 70vh; height: 25vh;" />
+</div>
