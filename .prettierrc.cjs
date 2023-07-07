3aef563d1498fce1315386c5635432ca27f8f4ee
Runo-Mikael Ojala <109177275+rmojala@users.noreply.github.com>
2023-07-08 02:43:16 +0300

    Migrate Prettier config to JS format

---
+++ .prettierrc.cjs
@@ -0,0 +1,18 @@
+/** @type {import("prettier").Options} */
+const config = {
+  plugins: [
+    "prettier-plugin-jsdoc",
+    "prettier-plugin-svelte",
+
+    //
+    // Must be the last one in the list
+    // See https://github.com/tailwindlabs/prettier-plugin-tailwindcss#compatibility-with-other-prettier-plugins
+    //
+    "prettier-plugin-tailwindcss",
+  ],
+
+  // See the above comment about the tailwindcss plugin
+  pluginSearchDirs: false,
+};
+
+module.exports = config;
