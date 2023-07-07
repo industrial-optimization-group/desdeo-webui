42589a0f1ced7fc275d3e0d22625ac3511678a63
Runo-Mikael Ojala <109177275+rmojala@users.noreply.github.com>
2023-07-08 02:29:32 +0300

    Migrate ESLint config to JS format

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,48 +2,48 @@
 // See also `.eslintignore`
 //
 
-{
+module.exports = {
   // Don't look for configuration files outside the project directory
-  "root": true,
+  root: true,
 
-  "plugins": [
+  plugins: [
     "@typescript-eslint",
     "eslint-plugin-svelte",
-    "eslint-plugin-tailwindcss"
+    "eslint-plugin-tailwindcss",
   ],
 
   //
   // Use the TypeScript parser as the default parser
   // See https://typescript-eslint.io/architecture/parser/#configuration
   //
-  "parser": "@typescript-eslint/parser",
-  "parserOptions": {
-    "extraFileExtensions": [".svelte"],
-    "project": ["./tsconfig.json"]
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    extraFileExtensions: [".svelte"],
+    project: ["./tsconfig.json"],
   },
 
   //
   // *.svelte files are parsed by the Svelte parser, which again
   // passes the embedded scripts to the TypeScript parser.
   //
-  "overrides": [
+  overrides: [
     {
-      "files": ["*.svelte"],
+      files: ["*.svelte"],
 
       // TODO: Should the parser options be adjusted?
-      "parser": "svelte-eslint-parser",
-      "parserOptions": {
+      parser: "svelte-eslint-parser",
+      parserOptions: {
         //
         // The TypeScript parser uses the options set above
         // See https://github.com/typescript-eslint/typescript-eslint/issues/6778
         //
-        "parser": "@typescript-eslint/parser"
-      }
-    }
+        parser: "@typescript-eslint/parser",
+      },
+    },
   ],
 
   // The linting rules
-  "extends": [
+  extends: [
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:svelte/recommended",
@@ -51,12 +51,12 @@
 
     // Turn off rules that conflict with Prettier
     "prettier",
-    "plugin:svelte/prettier"
+    "plugin:svelte/prettier",
   ],
 
   // See https://eslint.org/docs/latest/use/configure/language-options
-  "env": {
-    "browser": true,
-    "es2020": true
-  }
-}
+  env: {
+    browser: true,
+    es2020: true,
+  },
+};
