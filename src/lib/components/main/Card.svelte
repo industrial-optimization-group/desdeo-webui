fe2c4beb7a9e82905119c01be9426ea6781311e6
Runo-Mikael Ojala <109177275+rmojala@users.noreply.github.com>
2023-07-12 10:45:01 +0300

    Add card component

---
+++ src/lib/components/main/Card.svelte
@@ -0,0 +1,13 @@
+<div class="flex flex-col gap-2 bg-white p-4 shadow-md">
+  {#if $$slots.header}
+    <header class="mb-4 text-lg font-bold">
+      <slot name="header" />
+    </header>
+  {/if}
+  <slot />
+  {#if $$slots.footer}
+    <footer class="mt-4">
+      <slot name="footer" />
+    </footer>
+  {/if}
+</div>
