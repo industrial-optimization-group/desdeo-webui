87d29466c7a50d48e2b9712d112941f12f764313
Runo-Mikael Ojala <109177275+rmojala@users.noreply.github.com>
2023-07-18 05:00:52 +0300

    Update "how to cite" button

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -22,7 +22,7 @@
 </AppShell>
 
 <button
-  class="anchor variant-filled fixed bottom-0 right-0 p-2"
+  class="btn variant-filled-tertiary fixed bottom-0 right-0"
   on:click={() => {
     drawerStore.open();
   }}>How to cite DESDEO?</button
