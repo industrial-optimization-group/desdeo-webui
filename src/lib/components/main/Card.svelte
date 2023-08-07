f864900e464c03403ed3e3b8fc338046af9cc097
Runo-Mikael Ojala <109177275+rmojala@users.noreply.github.com>
2023-08-07 17:28:46 +0300

    Update card component

--- src/lib/components/main/Card.svelte
+++ src/lib/components/main/Card.svelte
@@ -1,4 +1,4 @@
-<div class="flex flex-col gap-2 bg-white p-4 shadow-md">
+<div class="flex flex-col gap-2 bg-white p-4 shadow-md {$$props.class ?? ''}">
   {#if $$slots.header}
     <header class="mb-4 text-lg font-bold">
       <slot name="header" />
