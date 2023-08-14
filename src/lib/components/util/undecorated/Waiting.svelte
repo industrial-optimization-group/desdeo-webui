a8ffcb67e3542e40c399f341fd9b0f4ebcaf7d0a
Runo-Mikael Ojala <109177275+rmojala@users.noreply.github.com>
2023-08-14 08:42:49 +0300

    Add waiting component

---
+++ src/lib/components/util/undecorated/Waiting.svelte
@@ -0,0 +1,16 @@
+<!--
+@component
+A simple animated circle to indicate an ongoing action.
+
+@slot label [An optional label to show next to the circle.]
+-->
+<script lang="ts">
+  import { ProgressRadial } from "@skeletonlabs/skeleton";
+</script>
+
+<div class="flex items-center gap-2">
+  <ProgressRadial stroke={200} width="w-6" />
+  {#if $$slots.label}
+    <slot name="label" />
+  {/if}
+</div>
