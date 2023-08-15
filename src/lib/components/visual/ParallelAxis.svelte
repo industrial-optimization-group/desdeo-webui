7d57628301b7fb589510fb0b2b87545052a820da
Eduard Ciriaco <65717975+edciriac@users.noreply.github.com>
2023-08-15 14:50:28 +0300

    Fix type errors

--- src/lib/components/visual/ParallelAxis.svelte
+++ src/lib/components/visual/ParallelAxis.svelte
@@ -25,7 +25,7 @@
   // Props for this component:
   export let values: number[][];
   export let minimize: boolean[];
-  export let showIndicators = true;
+  export let showIndicators = false;
   export let ranges: Ranges[] | undefined = undefined;
   export let names: string[] = []; // At the moment breaks the graphics if not given the same amount as values (objectives/axis)
   export let selectedIndices: number[] = [];
@@ -306,11 +306,13 @@
     return {
       color: colorPalette,
       tooltip: {
+        // TODO: Check if there is better way to achiece no type errors, check https://github.com/apache/echarts/issues/14723
         formatter: function (params) {
-          let result = params.name + "<br>";
-
-          for (let i = 0; i < params.data.value.length; i++) {
-            result += params.data.value[i] + "<br>";
+          let newParams: EChartOption.Tooltip.Format =
+            params as EChartOption.Tooltip.Format;
+          let result = newParams.name + "<br>";
+          for (let i = 0; i < newParams.data.value.length; i++) {
+            result += newParams.data.value[i] + "<br>";
           }
           return result;
         },
@@ -326,6 +328,9 @@
           type: "parallel",
           lineStyle: lineStyle,
           selectedMode: "multiple",
+          // TODO: Implement changing line color without using select. Now select causes type error, thus the ts-ignore. For some reason parallel axis does not have select property, but for example bar charts have:https://echarts.apache.org/en/option.html#series-bar.select, parallel: https://echarts.apache.org/en/option.html#series-parallel
+          //  eslint-disable-next-line @typescript-eslint/ban-ts-comment
+          // @ts-ignore -- Error says that disabled doesn't exist in the echarts series type, but in the documentation it exists. Might be because it's a new property, so they have not updated the type yet. https://echarts.apache.org/en/option.html#series-bar.emphasis.disabled
           select: {
             lineStyle: selectedLineStyle,
           },
@@ -365,9 +370,16 @@
     //   // console.log(this);
     //   console.log(params);
     // });
-    chart.on("click", function (params) {
-      selectedIndices = handleClickSelection(params, selectedIndices);
-    });
+    chart.on(
+      "click",
+      function (params: {
+        dataIndex: number;
+        componentType: string;
+        seriesIndex: number;
+      }) {
+        selectedIndices = handleClickSelection(params, selectedIndices);
+      }
+    );
   });
 </script>
 
