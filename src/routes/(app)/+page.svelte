04ab807b3e384285d8bdeacdf30e8980c2199051
Runo-Mikael Ojala <109177275+rmojala@users.noreply.github.com>
2023-08-23 08:18:57 +0300

    Fix redirection

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -1,1 +1,14 @@
+<script lang="ts">
+  import { login_status, LoginStatus } from "$lib/api";
+  import { goto } from "$app/navigation";
+
+  if ($login_status !== LoginStatus.LoggedOut) {
+    //
+    // The root page currently has no content to show, so we redirect
+    // logged in users to /saved_problems
+    //
+    goto("/saved_problems");
+  }
+</script>
+
 <h1>Root page</h1>
