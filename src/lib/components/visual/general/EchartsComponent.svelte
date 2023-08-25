d421d090bd2b330757caaa1bc6f59f81e42f9d4a
Eduard Ciriaco <65717975+edciriac@users.noreply.github.com>
2023-08-25 13:54:09 +0300

    Fix type error

--- src/lib/components/visual/general/EchartsComponent.svelte
+++ src/lib/components/visual/general/EchartsComponent.svelte
@@ -42,6 +42,9 @@
       chart.setOption(option);
       chart.setOption({
         color: colors,
+        // TODO: solve type error
+        //  eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore -- Error says that colorBy doesn't exist in the echarts series type, but in the documentation it exists. Might be because it's a new property (v.5.2.0), so they have not updated the type yet. https://echarts.apache.org/en/option.html#series-bar.colorBy
         colorBy: "data",
         animation: !disableAnimation,
       });
