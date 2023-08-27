9cd65ea009beac090d7f7d2d2ad86eed9b2df41b
Runo-Mikael Ojala <109177275+rmojala@users.noreply.github.com>
2023-08-27 21:51:55 +0300

    Add buttons slot to card

--- src/lib/components/main/Card.svelte
+++ src/lib/components/main/Card.svelte
@@ -1,7 +1,12 @@
 <div class="flex flex-col gap-2 bg-white p-4 shadow-md {$$props.class ?? ''}">
-  {#if $$slots.header}
-    <header class="mb-4 text-lg font-bold">
-      <slot name="header" />
+  {#if $$slots.header || $$slots.buttons}
+    <header class="mb-4 flex justify-between gap-4">
+      <span class="font-bold">
+        <slot name="header" />
+      </span>
+      <div class="flex gap-2">
+        <slot name="buttons" />
+      </div>
     </header>
   {/if}
   <slot />
