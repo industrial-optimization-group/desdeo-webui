8ac8037be47fc1b8198bbc77d727dbcd2e108a78
Samuli Kurikka <samuli.kurikka@outlook.com>
2023-12-05 18:12:52 +0200

    add ranks and wheights input

--- src/lib/components/main/Objective.svelte
+++ src/lib/components/main/Objective.svelte
@@ -1,7 +1,3 @@
-<!--
-@component
-A component for displaying objectives
--->
 <script lang="ts">
   import Card from "./Card.svelte";
   import type * as _ from "$lib/methods/nautilus/functional_api";
@@ -10,16 +6,28 @@ A component for displaying objectives
     name: string;
     minimize: boolean;
   }
+
   export let objectives: Objective[];
   export let method: _.Method;
 
   let showDetails = Array(objectives.length).fill(true);
+  let weights: Array<{ rank?: number; weight?: number }> = objectives.map(
+    () => ({})
+  );
 
-  // Add any additional logic here
   function toggleDetails(index: number) {
     showDetails[index] = !showDetails[index];
   }
-  console.log("objectives", objectives);
+
+  function updateWeight(index: number, type: "rank" | "weight", value: number) {
+    const otherType = type === "rank" ? "weight" : "rank";
+    weights[index][type] = value;
+    if (value != null) {
+      weights[index][otherType] = undefined; // Clear the other field
+    }
+  }
+
+  console.log("weights", weights);
   console.log("method", method);
 </script>
 
@@ -29,12 +37,13 @@ A component for displaying objectives
     {#each objectives as objective, j}
       <div class="objective-card" id={objective.name}>
         <div class="objective-header">
-          <h3>{objective.name} (Min)</h3>
-          <button on:click={() => toggleDetails(j)}> Hide Details </button>
+          <h3>{objective.name} {objective.minimize ? "(Min)" : "(Max)"}</h3>
+          <button on:click={() => toggleDetails(j)}
+            >{showDetails[j] ? "Hide" : "Show"} Details</button
+          >
         </div>
         {#if showDetails[j]}
           <div class="objective-details">
-            <!-- Repeat this for each piece of data you need to show -->
             <div class="data-point">
               <span>Objective:</span>
               <span>{objective.name}</span>
@@ -51,22 +60,35 @@ A component for displaying objectives
               <span>Nadir Point:</span>
               <span>{method.problem.nadir_point[j]}</span>
             </div>
-
-            <!-- ... Other data points ... -->
+            <div class="input-field">
+              <label for={`input-rank-${j}`}>Rank:</label>
+              <input
+                id={`input-rank-${j}`}
+                type="number"
+                min="1"
+                max="100"
+                placeholder="Rank"
+                bind:value={weights[j].rank}
+                on:input={() => updateWeight(j, "rank", weights[j]?.rank ?? 0)}
+              />
+            </div>
+            <div class="input-field">
+              <label for={`input-weight-${j}`}>Weight (%):</label>
+              <input
+                id={`input-weight-${j}`}
+                type="number"
+                min="0"
+                max="100"
+                placeholder="Weight"
+                bind:value={weights[j].weight}
+                on:input={() =>
+                  updateWeight(j, "weight", weights[j]?.weight ?? 0)}
+              />
+            </div>
           </div>
         {/if}
         <div class="objective-slider">
-          <label>
-            <input type="range" min="0" max="1" step="0.01" />
-            <div class="slider-marks">
-              <span class="best-value">0</span>
-              <span class="worst-value">1</span>
-            </div>
-          </label>
-        </div>
-        <div class="iterations-container">
-          <!-- Placeholder for iteration bars -->
-          <!-- You will generate these based on your data -->
+          <!-- Slider and iteration container as before -->
         </div>
       </div>
     {/each}
@@ -77,17 +99,66 @@ A component for displaying objectives
   .objectives-container {
     display: flex;
     flex-wrap: wrap;
-    gap: 10px;
+    gap: 20px;
   }
 
   .objective-card {
-    width: 400px;
-    /* Add any additional styles here */
+    border: 1px solid #ccc;
+    padding: 16px;
+    border-radius: 8px;
+    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+    background: #ffffff;
+    width: calc(50% - 10px); /* Adjust width for two cards per row with gap */
+  }
+
+  .objective-header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 12px;
+  }
+
+  .objective-details {
+    display: flex;
+    flex-direction: column;
+    gap: 8px;
+  }
+
+  .data-point {
+    display: flex;
+    justify-content: space-between;
+  }
+
+  .input-field {
+    display: flex;
+    flex-direction: column;
+    margin-top: 8px;
+  }
+
+  .input-field label {
+    font-size: 0.9em;
+    color: #333;
+    margin-bottom: 4px;
+  }
+
+  .input-field input[type="number"] {
+    width: 60px; /* Smaller width for numerical input */
+    padding: 8px;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    box-shadow: inset 0 1px 3px rgba(0, 0, 0, 0.1);
+  }
+
+  .objective-slider {
+    margin-top: 16px;
   }
 
   @media (max-width: 600px) {
     .objectives-container {
       flex-direction: column;
+    }
+    .objective-card {
+      width: 100%; /* Full width for mobile */
     }
   }
 </style>
