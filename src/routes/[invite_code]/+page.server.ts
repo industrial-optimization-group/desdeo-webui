33998d257f91d58e576af04a9deb9a291912299c
Hoang Bich Tram Mai <maihbt@jyu-mac-20240520.local>
2024-08-16 13:10:51 +0300

    Prerender invite-code page

--- src/routes/[invite_code]/+page.server.ts
+++ src/routes/[invite_code]/+page.server.ts
@@ -1,3 +1,9 @@
+import type { EntryGenerator } from './$types';
+
+export const entries: EntryGenerator = () => {
+  return [{ invite_code: 'fake-invite_code' }];
+};
+
 export function load({ params }) {
   return {
     invite_code: params.invite_code,
