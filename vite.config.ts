f0b8a117236d993978500986e4fddca85ab37d63
juropo <140176002+juropo@users.noreply.github.com>
2024-08-21 16:12:29 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import Icons from "unplugin-icons/vite";
-import { defineConfig } from "vitest/config";
+import { defineConfig } from "vite";
 
 // If you get "Plugin_2 is not assignable to PluginOption"
 // try deleting node_modules and reinstalling
@@ -16,3 +16,7 @@ export default defineConfig({
     include: ["src/**/*.{test,spec}.{js,ts}"],
   },
 });
+
+export default {
+  envPrefix: ['VITE_', 'PUBLIC_'] // This ensures both VITE_ and PUBLIC_ prefixed variables are exposed
+};
